3cec599f88813531c9cf2c02ea796dbf8f2b91c3
namji <skawlgns07@naver.com>
2024-02-07 21:13:20 +0900

    init commit

---
+++ src/test/java/com/pokemon/newsfeed/NewsfeedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pokemon.newsfeed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsfeedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
